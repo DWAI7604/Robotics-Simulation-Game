fcd4193f9027aa220059ce50417eeecb61b43f23
The Chair <d3vin213@gmail.com>
2025-03-15 08:43:48 -0700

    v0.6
    Made escape close the game.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -118,6 +118,11 @@ public class GameController : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
         if (Input.GetKey(KeyCode.R))
         {
             StaticPlayerColor = "Red";
